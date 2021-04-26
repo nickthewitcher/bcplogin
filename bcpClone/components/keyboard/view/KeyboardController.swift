f22c26559578a1f06621ad7f7ee305670d33d31b
Nik Quito Espinoza <nickquitoespinoza@gmail.com>
2021-04-26 18:48:28 -0500

    last changes

---
+++ bcpClone/components/keyboard/view/KeyboardController.swift
@@ -0,0 +1,40 @@
+//
+//  KeyboardController.swift
+//  bcpClone
+//
+//  Created by Nick Quito Espinoza on 26/04/21.
+//
+import UIKit
+import Foundation
+class KeyboardController: UIViewController{
+    @IBOutlet weak var oneKeyButton: UIButton!
+    
+    @IBOutlet weak var twoKeyButton: UIButton!
+    
+    
+    @IBOutlet weak var threeKeyButton: UIButton!
+    
+    @IBOutlet weak var fourKeyButton: UIButton!
+    
+    
+    @IBOutlet weak var fiveKeyButton: UIButton!
+    
+    @IBOutlet weak var sixKeyButton: UIButton!
+    
+    
+    @IBOutlet weak var sevenKeyButton: UIButton!
+    
+    
+    @IBOutlet weak var eightKeyButton: UIButton!
+    
+    
+    @IBOutlet weak var niveKeyButton: UIButton!
+    
+    
+    @IBOutlet weak var zeroKeyButton: UIButton!
+    
+    override func viewDidLoad() {
+       super.viewDidLoad()
+       // Do any additional setup after loading the view.
+   }
+}
