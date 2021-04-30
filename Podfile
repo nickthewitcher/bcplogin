db7421b7cf6c1d5f0949ae316886ae8dee64bfd2
Nik <nickquitoespinoza@gmail.com>
2021-04-30 12:38:49 -0500

    terminado teclado

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'bcpClone' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for bcpClone
+
+  target 'bcpCloneTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'bcpCloneUITests' do
+    # Pods for testing
+  end
+
+end
--- bcpClone/components/keyboard/view/KeyboardController.swift
+++ bcpClone/components/keyboard/view/KeyboardController.swift
@@ -7,34 +7,38 @@
 import UIKit
 import Foundation
 class KeyboardController: UIViewController{
-    @IBOutlet weak var oneKeyButton: UIButton!
+  
+    @IBOutlet weak var pos1: UIButton!
+    @IBOutlet weak var pos2: UIButton!
     
-    @IBOutlet weak var twoKeyButton: UIButton!
+    @IBOutlet weak var pos3: UIButton!
+    @IBOutlet weak var pos4: UIButton!
+    @IBOutlet weak var pos5: UIButton!
+    @IBOutlet weak var pos6: UIButton!
+    @IBOutlet weak var pos7: UIButton!
+    @IBOutlet weak var pos8: UIButton!
+    @IBOutlet weak var pos9: UIButton!
+    @IBOutlet weak var posDelete: UIButton!
+    var listaNumber = [Int](){
+        didSet{
+            self.pos1.setTitle(String(listaNumber[0]), for: .normal)
+            self.pos2.setTitle(String(listaNumber[1]), for: .normal)
+            self.pos3.setTitle(String(listaNumber[2]), for: .normal)
+            self.pos4.setTitle(String(listaNumber[3]), for: .normal)
+            self.pos5.setTitle(String(listaNumber[4]), for: .normal)
+            self.pos6.setTitle(String(listaNumber[5]), for: .normal)
+            self.pos7.setTitle(String(listaNumber[6]), for: .normal)
+            self.pos8.setTitle(String(listaNumber[7]), for: .normal)
+            self.pos9.setTitle(String(listaNumber[8]), for: .normal)
+
+ }}
     
-    
-    @IBOutlet weak var threeKeyButton: UIButton!
-    
-    @IBOutlet weak var fourKeyButton: UIButton!
-    
-    
-    @IBOutlet weak var fiveKeyButton: UIButton!
-    
-    @IBOutlet weak var sixKeyButton: UIButton!
-    
-    
-    @IBOutlet weak var sevenKeyButton: UIButton!
-    
-    
-    @IBOutlet weak var eightKeyButton: UIButton!
-    
-    
-    @IBOutlet weak var niveKeyButton: UIButton!
-    
-    
-    @IBOutlet weak var zeroKeyButton: UIButton!
+
     
     override func viewDidLoad() {
        super.viewDidLoad()
+        self.listaNumber = [9,2,3,4,5,6,7,8,1]
        // Do any additional setup after loading the view.
    }
+    
 }
